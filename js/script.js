2053fea18cb983cb1e2b5269105300bb1a6d6e65
Дмитрий <3700010@gmail.com>
2017-02-10 19:27:12 +0300

    Завершение.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
-var controls = document.querySelector('.order-form');
-controls.classList.add('order-form-none');
-
-
 var buttonSearch = document.querySelector('.open-form'),
     formSearch = document.querySelector('.order-form');
 
 buttonSearch.addEventListener('click', openForm);
 function openForm(e){
-  e.preventDefault;
-  formSearch.classList.remove('order-form-none');
+  e.preventDefault();
+  formSearch.classList.toggle('order-form-none');
 }
