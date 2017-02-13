227974b3bf127d52b9452d65db74f8cdfc512673
Дмитрий <3700010@gmail.com>
2017-02-13 13:07:37 +0300

    js в форме.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
-var buttonSearch = document.querySelector('.open-form'),
-    formSearch = document.querySelector('.order-form');
+var buttonSearch = document.querySelector('.open-form');
+var formSearch = document.querySelector('.order-form');
+var form = document.querySelector("form");
+var come = document.querySelector("[name=date-come]");
+var exit = document.querySelector("[name=date-exit]");
+var adults = document.querySelector("[name=all-adults]");
+var children = document.querySelector("[name=all-children]");
+var storageAdults = localStorage.getItem("adults");
+var storageChildren = localStorage.getItem("children");
 
 buttonSearch.addEventListener('click', openForm);
 function openForm(e){
   e.preventDefault();
-  formSearch.classList.toggle('order-form-none');
+
+formSearch.classList.toggle('order-form-none');
+
+if (storageAdults || storageChildren) {
+  adults.value = storageAdults;
+  children.value = storageChildren;
+  }
 }
+
+form.addEventListener("submit", function(event) {
+  if (!come.value || !exit.value || !adults.value || !children.value) {
+    event.preventDefault();
+      alert("Пожалуйста, заполните все поля!");
+  } else {
+    localStorage.setItem("storageAdults", adults.value);
+    localStorage.setItem("storageChildren", children.value);
+    alert("Спасибо, Ваша заявка принята!");
+  }
+});
